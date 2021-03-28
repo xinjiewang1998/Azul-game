fba41bf46bf9a4a5c961c0a9b827f25e58c4b1e8
u7224266 <u7224266@anu.edu.au>
2021-03-28 18:32:21 +0800

    Completed part of D2B at home.

--- src/comp1110/ass2/Game.java
+++ src/comp1110/ass2/Game.java
@@ -3,7 +3,11 @@ package comp1110.ass2;
 public class Game {
 
     public static void main(String[] args) {
-
+        // initialise common
+        // initialise players
+        // run first turn
+        // run second turn
+        // ...
     }
 
 }
--- src/comp1110/ass2/Player.java
+++ src/comp1110/ass2/Player.java
@@ -1,5 +1,75 @@
 package comp1110.ass2;
 
-public class Player {
+import comp1110.ass2.board.Board;
+import comp1110.ass2.common.Common;
 
+import java.util.ArrayList;
+
+public class Player {
+    Board board;
+    Common common;
+
+    Tile firstPlayer;
+    boolean isFirstPlayer;
+    Player nextPlayer;
+
+    String playerState;
+
+    public Player(Common common, boolean isFirstPlayer){
+        this.board = new Board();
+        this.common = common;
+        this.isFirstPlayer = isFirstPlayer;
+        if (isFirstPlayer){
+            //acquire tile from centre
+            ArrayList<Tile> tiles = common.getCentre().takeTilesFromCentre("firstPlayer");
+            this.firstPlayer = tiles.get(0);
+        }
+    }
+
+    public void setPlayerState(String playerState){
+        this.playerState = playerState;
+    }
+
+    public void setNextPlayer(Player nextPlayer){
+        this.nextPlayer = nextPlayer;
+    }
+
+    public Player getNextPlayer(){
+        return this.nextPlayer;
+    }
+
+    /**
+     * decode playerStateString.
+     */
+    public void decodePlayerState(){
+        //fixme
+    }
+
+    /**
+     * encode player state to playerState string
+     * by concateenate mutiple toString methods.
+     * @return playerState
+     */
+    public String encodePlayerState(){
+        //fixme
+        return "";
+    }
+
+    /**
+     * according to the instruction string given,
+     * pass it to board.performInstruction method.
+     * @param instruction
+     */
+    public void play(String instruction){
+        //fixme
+    }
+
+    /**
+     * fetch the score from board.
+     * @return the score
+     */
+    public int getScore(){
+        //fixme
+        return 0;
+    }
 }
