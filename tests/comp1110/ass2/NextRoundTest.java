5ad75a6f484846b35e8bf21e2962eec6a4fd3415
Josh Milthorpe <josh.milthorpe@anu.edu.au>
2021-04-16 09:24:35 +1000

    Fix non-well-formed states in NextRoundTest and GetBonusPointsTest
    Clarify error message in notNextRound Test factory-centre string specifically rather than entire shared state string

--- tests/comp1110/ass2/NextRoundTest.java
+++ tests/comp1110/ass2/NextRoundTest.java
@@ -21,7 +21,7 @@ public class NextRoundTest {
             new String[]{"BFCB0907091213D0000000000", "A19Me00a01c03b04b10e11d12a14d20c21a22b23e24a30d31c32b42c44S4e2FbbbbbccB11Ma00c01e02d04b11d12e13a14d20a22b24c30d31b33a41d43S3a3Fbcccf"},
             new String[]{"BFCB0805060805D0405030709", "A36Me01d02c03b04a10d11b12e13c14c20b21d23e30S2e13c14a2FabbbfB12Mc00e02a03d04a10e11b12c14c21a22b24d42b43S3c3Fabc"},
             new String[]{"AFCB0001040100D1210051208", "A0Mc00d02e03a04b10e11c13b22c24a30b31e40d44S1a13c34a1FbbdeeeeB18Mc00b01a02d03e04d10b12a13c14e22c23a24e33e41c42b44S1e12b13a1Fcddf"},
-            new String[]{"BFCB0000060103D1111050511", "A15Ma00c01e02d03b04b12e13a14d20b21a23e24e30c33d34c40S1c12c1FaaafB7Mb00c01e02a04d10b12c14c20e21a23b24d33a40d41c42S3b3Fdddddddd"},
+            new String[]{"BFCB0000060103D1111050511", "A15Ma00c01e02d03b04b12e13a14d20b21a23e24e30c33d34c40S1c12c1FaaafB7Mb00c01e02a04d10b12c14c20e21a23b24d33a40d41c42S3b3Fddddddd"},
             new String[]{"AFCB0703080106D0411011105", "A7Md00a02b03e04d11e13a20c24c30e40S1c12b2FaaaaB11Ma00c01b02d03e04c12a13b14d20b21c23a32S1e13c34e4Fccddddf"},
             new String[]{"BFCB0606060308D0407080801", "A29Me00a01c02b04d10c11e13a14b20d22a23c24a40S3e24b2FafB12Ma00d01b02e04b10a11c12e13d14d23b24c43S3e14e4Faaacdddd"},
             new String[]{"AFCB0201050507D1011040204", "A7Mb01c02e03a04e10a12b13d21b22c23d30c31c44S1c13b14d4FaaadddfB14Mc00e01d02b03a04a11d13d20c22a23b24e34d41e43S1c13c14c2Fbbeeee"},
@@ -34,7 +34,7 @@ public class NextRoundTest {
             new String[] {"BFCfB0907091213D0006050000", "A7Me00a01c03b04b10e11d12a14d20c21a22b23e24a30d31c32b42c44S4e2FB13Ma00c01e02d04b11d12e13a14d20a22b24c30d31b33a41d43S3a3F"},
             new String[] {"AFCfB0805060805D0609040709", "A30Me01d02c03b04a10d11b12e13c14c20b21d23e30S2e13c14a2FB8Mc00e02a03d04a10e11b12c14c21a22b24d42b43S3c3F"},
             new String[] {"BFCfB0001040100D1212061512", "A0Mc00d02e03a04b10e11c13b22c24a30b31e40d44S1a13c34a1FB14Mc00b01a02d03e04d10b12a13c14e22c23a24e33e41c42b44S1e12b13a1F"},
-            new String[] {"AFCfB0000060103D1411051311", "A11Ma00c01e02d03b04b12e13a14d20b21a23e24e30c33d34c40S1c12c1FB0Mb00c01e02a04d10b12c14c20e21a23b24d33a40d41c42S3b3F"},
+            new String[] {"AFCfB0000060103D1411051211", "A11Ma00c01e02d03b04b12e13a14d20b21a23e24e30c33d34c40S1c12c1FB0Mb00c01e02a04d10b12c14c20e21a23b24d33a40d41c42S3b3F"},
             new String[] {"BFCfB0703080106D0811031505", "A1Md00a02b03e04d11e13a20c24c30e40S1c12b2FB2Ma00c01b02d03e04c12a13b14d20b21c23a32S1e13c34e4F"},
             new String[] {"AFCfB0606060308D0807091201", "A27Me00a01c02b04d10c11e13a14b20d22a23c24a40S3e24b2FB2Ma00d01b02e04b10a11c12e13d14d23b24c43S3e14e4F"},
             new String[] {"AFCfB0201050507D1313040508", "A0Mb01c02e03a04e10a12b13d21b22c23d30c31c44S1c13b14d4FB5Mc00e01d02b03a04a11d13d20c22a23b24e34d41e43S1c13c14c2F"},
@@ -131,16 +131,18 @@ public class NextRoundTest {
             assertNotNull(out, "Azul.nextRound({\"" + state[0] + "\", \"" + state[1] + "\"})"
                     + System.lineSeparator()
                     + "returned null");
-
             String errorMessagePrefix = errorPrefix(state, out);
-            assertEquals(state[0], out[0], errorMessagePrefix + "factories were not empty so the round has not finished.");
+            assertEquals(state[0], out[0], errorMessagePrefix + "factories were not empty or storage row(s) were full, so the round has not finished.");
         }
         String[] state = new String[2];
         state[0] = ExampleGames.FULL_GAME_WITH_MOVES[13][0];
         state[1] = ExampleGames.FULL_GAME_WITH_MOVES[13][1];
         String[] out = Azul.nextRound(state.clone());
+        String stateFC = state[0].substring(state[0].indexOf('F'), state[0].indexOf('B'));
+        String outFC = out[0].substring(1);
+        outFC = outFC.substring(outFC.indexOf('F'), outFC.indexOf('B'));
         String errorMessagePrefix = errorPrefix(state, out);
-        assertNotEquals(state[0], out[0], errorMessagePrefix + "end of round, expected factories to be refilled.");
+        assertNotEquals(stateFC, outFC, errorMessagePrefix + "end of round, expected factories to be refilled.");
     }
 
     @Test
